cf1cf3afc514cf774ab47c04276bff03ad27a352
ducbang1310 <ducb1310@gmail.com>
2025-03-18 23:39:25 +0700

    nguen

--- Spa_prj_haiz/Form1.Designer.cs
+++ Spa_prj_haiz/Form1.Designer.cs
@@ -28,18 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Spa_prj_haiz/Form1.cs
+++ Spa_prj_haiz/Form1.cs
@@ -16,5 +16,10 @@ namespace Spa_prj_haiz
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
